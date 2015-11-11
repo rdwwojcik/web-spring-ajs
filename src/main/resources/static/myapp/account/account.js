47a778feec327b59dae6bab1a1596aabf7057a98
rdwwojcik <rdwwojcik@gmail.com>
2015-11-11 22:22:18 +0100

    Display json

--- src/main/resources/static/myapp/account/account.js
+++ src/main/resources/static/myapp/account/account.js
@@ -11,6 +11,12 @@
                 controller:     'AccountCtrl',
                 controllerAs:   'accountCtrl',
                 data: {pageTitle: 'Account'}
+            }).
+            when('/newaccount', {
+                templateUrl: 'myapp/account/new_account.html',
+                controller:     'AccountCtrl',
+                controllerAs:   'accountCtrl',
+                data: {pageTitle: 'New Account'}
             });
     }]);
 
@@ -22,15 +28,15 @@
 
         session.login = function(data){
             alert('User Logged ' + data.name + " and " + data.password);
-          localStorage.setItem("session", data);
+            localStorage.setItem("session", data);
         };
 
         session.logout = function(){
-          localStorage.removeItem("session");
+            localStorage.removeItem("session");
         };
 
         session.isLoggedIn = function(){
-          localStorage.getItem("session") !== null;
+            localStorage.getItem("session") !== null;
         };
 
         return session;
@@ -58,45 +64,71 @@
                 failure);
         };
 
-        return service;
-    });
-
-    app.factory('messageService', function(){
-
-        return {
-            comments: []
+        service.getAccounts = function(success){
+            console.log('Call getAccounts');
+            var accounts = [];
+            var Account = $resource('http://localhost:8080/rest/accounts');
+            var data = Account.get(
+                function(){
+                    accounts = data.accounts;
+                    success(accounts);
+                }
+            );
         };
 
+        return service;
     });
 
     app.controller('AccountCtrl', function HomeController($scope, sessionService, accountService) {
 
         this.wiadomosc = 'info2';
-
         this.login = function(){
-            accountService.userExist($scope.account, function(account){
+            accountService.userExist($scope.account,
+                function(account){
                     sessionService.login(account);
-                    console.log(account.name);
-            },
-            function(){
-                alert("Error logging in user");
-            });
-
-            var dane = localStorage.getItem("session");
-            this.wiadomosc = dane.name;
+                },
+                function(){
+                    alert("Error logging in user");
+                });
         };
 
-        //this.login = function(){
-        //    accountService.register($scope.product,
-        //        function (returnedData) {
-        //            sessionService.login(returnedData);
-        //        },
-        //        function(){
-        //            alert("Error registering user");
-        //        }
-        //    );
-        //    //sessionService.login($scope.product);
-        //};
+        $scope.informacja = 'informacja';
+        this.accountsList = function(){
+            console.log('Call accounts function');
+            accountService.getAccounts(
+                function(accounts_data){
+                    console.log('Call success');
+                    $scope.informacja = 'informacja druga';
+                    $scope.accounts_table = accounts_data;
+                    console.log($scope.accounts_table);
+                }
+            );
+        };
     });
 
+
+    //app.service('acService', function($http, $q){
+    //    var deferred = $q.defer();
+    //    $http.get('http://localhost:8080/rest/accounts').then(function(data){
+    //        deferred.resolve(data);
+    //    });
+    //
+    //    this.getAccount = function(){
+    //        console.log('Call getAccount');
+    //        return deferred.promise;
+    //    };
+    //});
+
+    //this.accountsList = function(){
+        //var promise = acService.getAccount();
+        //promise.then(function(data){
+        //    console.log('Call promise');
+        //    $scope.accounts_table = data.data.accounts;
+        //    console.log($scope.accounts_table);
+        //    $scope.informacja = 'informacja trzecia';
+        //});
+        //
+        ////console.log('Call info');
+        //$scope.informacja = 'informacja druga';
+    //};
 })();
